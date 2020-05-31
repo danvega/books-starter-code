5bbe89f4f9e7c69432a020916a86fc9b60c6d344
Dan Vega <danvega@gmail.com>
2020-05-31 13:52:48 -0400

    removing methods from the controller, not needed here

--- src/main/java/com/oreilly/books/BookController.java
+++ src/main/java/com/oreilly/books/BookController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/books")
@@ -24,22 +23,4 @@ public class BookController {
         return books;
     }
 
-    @PostMapping
-    public void create(@RequestBody Map<String, String> payload) {
-        books.add(payload.get("title"));
-    }
-
-    @PutMapping
-    public void update(@RequestBody Map<String, String> payload) {
-        if (books.contains(payload.get("oldTitle"))) {
-            books.set(books.indexOf(payload.get("oldTitle")), payload.get("newTitle"));
-        }
-    }
-
-    @DeleteMapping
-    public void delete(@RequestParam String title) {
-        if (books.contains(title)) {
-            books.remove(books.indexOf(title));
-        }
-    }
 }
